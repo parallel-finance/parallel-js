7a5756877009d660565cf88da16c0e3f5fc98873
Yang Zhou <deedecho@gmail.com>
2021-07-24 23:58:47 +0800

    export parallel types

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -22,6 +22,7 @@ export const options = ({
   ...otherOptions
 }: ApiOptions = {}): ApiOptions => ({
   types: {
+    ...parallelTypes,
     ...types
   },
   rpc: {
---
+++ packages/types/src/interfaces/augment-api.ts
@@ -0,0 +1,5 @@
+import '@polkadot/api/augment/rpc';
+import './augment-api-consts';
+import './augment-api-query';
+import './augment-api-rpc';
+import './augment-api-tx';
