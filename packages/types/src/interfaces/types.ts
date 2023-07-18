c0ac91fba43291337bd5772671f221b34407b7f6
MrPai <1164934857@qq.com>
2023-07-18 14:51:53 +0800

    manually add into types

--- packages/types/src/interfaces/types.ts
+++ packages/types/src/interfaces/types.ts
@@ -1,3 +1,11 @@
 // Auto-generated via `yarn polkadot-types-from-defs`, do not edit
 /* eslint-disable */
 
+export * from './liquidStaking/types';
+export * from './farming/types';
+export * from './loans/types';
+export * from './amm/types';
+export * from './prices/types';
+export * from './primitives/types';
+export * from './router/types';
+export * from './runtime/types';
